7af1d0b3c28fedcab47a71cf2ba7a93163e2ae38
Christopher Murray <cmurray1105@gmail.com>
2021-01-07 22:41:46 -0600

    formatting

--- server.compiled.js
+++ server.compiled.js
@@ -6,24 +6,24 @@ var _express = _interopRequireDefault(require("express"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var db = require('./db/queries');
+var db = require("./db/queries");
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
 var PORT = process.env.HTTP_PORT || 8080;
 var app = (0, _express["default"])();
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
+app.use(_express["default"]["static"](_path["default"].join(__dirname, "client", "build")));
 app.use(bodyParser.json());
-app.get('/', function (req, res) {
-  res.send('just gonna send it');
+app.get("/", function (req, res) {
+  res.send("just gonna send it");
 });
-app.get('/flower', function (req, res) {
+app.get("/flower", function (req, res) {
   res.json({
-    name: 'Dandelion',
-    colour: 'Blue-ish'
+    name: "Dandelion",
+    colour: "Blue-ish"
   });
 });
-app.get('/products', function (req, res) {
+app.get("/products", function (req, res) {
   // console.log('server', req.query.product)
   db.getProducts(req.query.product, function (err, result) {
     if (err) {
@@ -34,11 +34,11 @@ app.get('/products', function (req, res) {
     }
   });
 });
-app.post('/addOrder', function (req, res) {
-  console.log('query', req.body);
-  var currentDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  console.log("del date", req.body.deliveryDate.slice(0, 19).replace('T', ' '));
-  var order = [req.body.address, req.body.customerName, 'Liberty Hill', currentDate, req.body.deliveryDate.slice(0, 19).replace('T', ' '), req.body.neighborhood, req.body.email, req.body.phone]; // console.log("order:", order)
+app.post("/addOrder", function (req, res) {
+  console.log("query", req.body);
+  var currentDate = new Date().toISOString().slice(0, 19).replace("T", " ");
+  console.log("del date", req.body.deliveryDate.slice(0, 19).replace("T", " "));
+  var order = [req.body.address, req.body.customerName, "Liberty Hill", currentDate, req.body.deliveryDate.slice(0, 19).replace("T", " "), req.body.neighborhood, req.body.email, req.body.phone]; // console.log("order:", order)
 
   var id;
   db.addDelivery(order, function (err, results) {
@@ -48,14 +48,14 @@ app.post('/addOrder', function (req, res) {
       // console.log("RESULT FROM ADDING", results.insertId)
       var _id = results.insertId;
 
-      for (item in req.body.cartItems) {
+      for (var item in req.body.cartItems) {
         var params = [req.body.cartItems[item].id, _id, req.body.cartItems[item].quantity];
         db.addItemToOrder(params, function (err, secondResult) {
           // console.log("callback called")
           if (err) {
             res.send(err);
           } else {
-            console.log('sent: ', secondResult);
+            console.log("sent: ", secondResult);
           }
         });
       }
@@ -64,9 +64,8 @@ app.post('/addOrder', function (req, res) {
     }
   });
 });
-app.post('/updateQuantity', function (req, res) {
-  console.log(req.body.quantity, req.body.productName); // let quantity = [req.body.quantity, req.body.productName]
-
+app.post("/updateQuantity", function (req, res) {
+  console.log(req.body.quantity, req.body.productName);
   var quantity = [req.body.quantity, req.body.productName];
   db.updateQuantity(quantity, function (err, result) {
     if (err) {
@@ -76,10 +75,9 @@ app.post('/updateQuantity', function (req, res) {
     }
   });
 });
-app.get('/getOrders', function (req, res) {
+app.get("/getOrders", function (req, res) {
   db.getOrders(function (err, result) {
-    console.log("RESULTTTTT1", result);
-    result = result[0];
+    console.log("RESULTTTTT1", result); // result = result[0]
 
     if (err) {
       console.log(err);
@@ -88,28 +86,28 @@ app.get('/getOrders', function (req, res) {
 
       var orders = {};
 
-      for (var _item in result) {
-        console.log("ITEM: ", result[_item]);
+      for (var item in result) {
+        console.log("ITEM: ", result[item]);
 
-        if (!orders[result[_item].id]) {
-          orders[result[_item].id] = {
-            customer: result[_item].name,
-            address: result[_item].street_address,
-            deliveryDate: result[_item].scheduled_delivery,
-            neighborhood: result[_item].neighborhood,
-            email: result[_item].email,
-            phone: result[_item].phone,
+        if (!orders[result[item].id]) {
+          orders[result[item].id] = {
+            customer: result[item].name,
+            address: result[item].street_address,
+            deliveryDate: result[item].scheduled_delivery,
+            neighborhood: result[item].neighborhood,
+            email: result[item].email,
+            phone: result[item].phone,
             products: [{
-              productName: result[_item].product_name,
-              quantity: result[_item].quantity,
-              price: result[_item].price
+              productName: result[item].product_name,
+              quantity: result[item].quantity,
+              price: result[item].price
             }]
           };
         } else {
-          orders[result[_item].id].products.push({
-            productName: result[_item].product_name,
-            quantity: result[_item].quantity,
-            price: result[_item].price
+          orders[result[item].id].products.push({
+            productName: result[item].product_name,
+            quantity: result[item].quantity,
+            price: result[item].price
           });
         }
       }
