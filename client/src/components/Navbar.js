9b8c29ca9608232d2f6baeceb42067751f6ef258
Christopher Murray <cmurray1105@gmail.com>
2021-02-03 23:22:46 -0600

    added another break line

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,18 @@
 import { makeStyles } from "@material-ui/core/styles";
 // import Popper from '@material-ui/core/popper';
-import React, {ReactElement} from "react";
+import React, { ReactElement } from "react";
 import TableChartIcon from "@material-ui/icons/TableChart";
-import { ClickAwayListener } from '@material-ui/core';
+import { ClickAwayListener } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 // import "./styles.css";
-import CartPopper from './CartPopper';
-import CheckoutModal from './CheckoutModal'
-import CartItem from './CartItem'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-function Navbar (props) {
+import CartPopper from "./CartPopper";
+import CheckoutModal from "./CheckoutModal";
+import CartItem from "./CartItem";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+function Navbar(props) {
   const [open, setOpen] = React.useState(false);
-  const [height, setHeight] = React.useState('400px');
+  const [height, setHeight] = React.useState("400px");
   const [anchorEl, handleAnchor] = React.useState(null);
   const [arrowRef, handleArrowRef] = React.useState(null);
   const useStyles = makeStyles((theme) => ({
@@ -22,59 +22,57 @@ function Navbar (props) {
       padding: theme.spacing(1),
       backgroundColor: theme.palette.background.paper,
       zIndex: 3,
-      height: 'max-content',
-      minHeight: '400px',
-      width: '500px',
-      borderRadius: '6px',
-      margin:'auto',
-      display: 'grid',
-      gridTemplateColumns: '1fr 1fr 1fr',
-      gridTemplateRows: '4fr 1fr',
-      gridTemplateAreas: " 'side list side'  'side checkout side"
+      height: "max-content",
+      minHeight: "400px",
+      width: "500px",
+      borderRadius: "6px",
+      margin: "auto",
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr 1fr",
+      gridTemplateRows: "4fr 1fr",
+      gridTemplateAreas: " 'side list side'  'side checkout side",
     },
-    buttonContainer:{
-      display: 'flex',
-      flexDirection: 'row',
-      textAlign: 'center',
-      marginLeft:'85%'
+    buttonContainer: {
+      display: "flex",
+      flexDirection: "row",
+      textAlign: "center",
+      marginLeft: "85%",
     },
     button: {
-      height: '38px',
-      width:'38px',
-      background: 'white',
-      border: 'none',
-      color: 'red',
-      cursor: 'pointer',
-      textAlign: 'center',
-      display: 'inline-block',
-      outline: 'none'
+      height: "38px",
+      width: "38px",
+      background: "white",
+      border: "none",
+      color: "red",
+      cursor: "pointer",
+      textAlign: "center",
+      display: "inline-block",
+      outline: "none",
     },
     cartContainer: {
-      gridArea: 'list',
+      gridArea: "list",
     },
     checkoutArea: {
-      gridArea: 'checkout'
+      gridArea: "checkout",
     },
     total: {
-      height: '64px'
-    }
-  })
-  )
+      height: "64px",
+    },
+  }));
   const classes = useStyles();
 
-
   const node = React.useRef();
-  console.log("nodey node:", node)
+  console.log("nodey node:", node);
 
   const handleClick = (event) => {
     handleAnchor(anchorEl ? null : event.currentTarget);
-    console.log("ANCHORS AWAY!", anchorEl)
-    setOpen(!open)
+    console.log("ANCHORS AWAY!", anchorEl);
+    setOpen(!open);
   };
 
-  let items = []
-  for (let order in props.cartItems){
-    items.push(props.cartItems[order])
+  let items = [];
+  for (let order in props.cartItems) {
+    items.push(props.cartItems[order]);
   }
 
   // if (Object.keys(props.cartItems).length > 4){
@@ -83,78 +81,74 @@ function Navbar (props) {
   //   setHeight(newHeight)
   // }
 
-  return(
-
+  return (
     // <ClickAwayListener onClickAway={handleClickAway}>
-    <div >
-    <div className={classes.buttonContainer} >
-                  <button
-                  className={classes.button}
-                  onClick={handleClick}
-                  >
-                  <FontAwesomeIcon icon={faShoppingCart} size='2x'/>
-                  <span>{props.quantity}</span>
-                </button>
+    <div>
+      <div className={classes.buttonContainer}>
+        <button className={classes.button} onClick={handleClick}>
+          <FontAwesomeIcon icon={faShoppingCart} size="2x" />
+          <span>{props.quantity}</span>
+        </button>
+      </div>
+      <CartPopper
+        ref={node}
+        placement="top"
+        open={open}
+        anchorEl={anchorEl}
+        modifiers={{
+          flip: {
+            enabled: true,
+          },
+          arrow: {
+            enabled: true,
+            element: arrowRef,
+          },
+          preventOverflow: {
+            enabled: "true",
+            boundariesElement: "scrollParent",
+          },
+        }}
+      >
+        {true && <span className="arrow" ref={handleArrowRef} />}
+        <div className={classes.paper}>
+          <div className="cartContainer">
+            {/* MAP CART ITEMS OR DISPLAY EMPTY CART */}
+            {items.map((item) => (
+              <>
+                <CartItem
+                  increaseQuantity={props.increaseQuantity}
+                  decreaseQuantity={props.decreaseQuantity}
+                  products={props.products}
+                  item={item}
+                  convertPriceToString={props.convertPriceToString}
+                />
+                <hr />
+              </>
+            ))}
+          </div>
+
+          <div className="checkoutArea">
+            {/* IF CART IS NOT EMPTY RENDER CHECKOUTMODAL BUTTON*/}
+            {props.quantity > 0 ? (
+              <>
+                <div className={classes.total}>
+                  <hr />
+                  {("NAVBAR PROP", props.total)}
+                  <hr />
                 </div>
-                    <CartPopper
-                  ref={node}
-                  placement="top"
-                  open={open}
-                  anchorEl={anchorEl}
-                  modifiers={{
-                    flip: {
-                      enabled: true,
-                    },
-                    arrow: {
-                      enabled: true,
-                      element: arrowRef,
-                    },
-                    preventOverflow: {
-                      enabled: "true",
-                      boundariesElement: 'scrollParent'
-                    },
-                  }}
-                >
-                  {
-                    true &&
-                    <span className="arrow" ref={handleArrowRef} />
-                  }
-                  <div className={classes.paper}>
-                  <div className="cartContainer">
-{/* MAP CART ITEMS OR DISPLAY EMPTY CART */}
-{
-  items.map((item)=>(
-    <>
-    <CartItem
-      increaseQuantity={props.increaseQuantity}
-      decreaseQuantity={props.decreaseQuantity}
-      products={props.products}
-      item={item}
-      convertPriceToString={props.convertPriceToString}
-      />
-    <hr/>
-    </>
-  ))
-  }
-                  </div>
-
-                  <div className="checkoutArea">
-{/* IF CART IS NOT EMPTY RENDER CHECKOUTMODAL BUTTON*/}
-{props.quantity > 0 ?
-<>
-  <div className={classes.total}>
-                  {console.log("NAVBAR PROP", props)}
-<hr/>
-</div>
-                    <CheckoutModal clearOrder={props.clearOrder} total={props.total} products={props.products} cartItems={props.cartItems} />
-                    </>
-: null }
-</div>
-
-                  </div>
-                </CartPopper>
+                <CheckoutModal
+                  clearOrder={props.clearOrder}
+                  total={props.total}
+                  products={props.products}
+                  cartItems={props.cartItems}
+                />
+              </>
+            ) : null}
+          </div>
+        </div>
+      </CartPopper>
     </div>
-                // </ClickAwayListener>
-  )
+    // </ClickAwayListener>
+  );
 }
 export default Navbar;
